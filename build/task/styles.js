e0be0cb26d284d5f60416298ffa006a1e664d0d7
JieSwift <771299500@qq.com>
2017-11-21 19:14:03 +0800

    :arrow_up: 更新gulp-minify-css为gulp-clean-css。

--- build/task/styles.js
+++ build/task/styles.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass'); //编译sass
 var concat = require('gulp-concat'); //合并文件
 var rename = require('gulp-rename'); //重命名文件
 var autoprefixer = require('gulp-autoprefixer'); //补全css前缀
-var minifycss = require('gulp-minify-css'); //压缩css
+var minifycss = require('gulp-clean-css'); //压缩css
 var notify = require('gulp-notify'); //通知
 var rev = require('gulp-rev'); //- 对文件名加MD5后缀
 
@@ -18,7 +18,7 @@ gulp.task('sass-dev', function() {
 		.pipe(sass({ outputStyle: 'compressed' }))
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
 		.pipe(rename({ suffix: '.min' }))
-		.pipe(minifycss())
+		.pipe(minifycss({ outputStyle: 'compressed' }))
 		.pipe(gulp.dest(STYLES_DIST));
 });
 
@@ -28,7 +28,7 @@ gulp.task('sass-pro', function() {
 		.pipe(sass({ outputStyle: 'compressed' }))
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
 		.pipe(rename({ suffix: '.min' }))
-		.pipe(minifycss())
+		.pipe(minifycss({ outputStyle: 'compressed' }))
 		.pipe(rev())
 		.pipe(gulp.dest(STYLES_DIST))
 		.pipe(rev.manifest())
